19e7bdf80e33f722362bad40f2cbf3525dea9a58
Loay Mohamed <loaymohamed1999@gmail.com>
2020-05-16 21:24:38 +0200

    UI updated..

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //setStyleSheet("background-color:white;");
+    setAutoFillBackground( true );
 }
 
 MainWindow::~MainWindow()
